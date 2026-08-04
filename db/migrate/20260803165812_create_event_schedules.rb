42b8e1c2d4d25992e23634b76e6ca50b6460843c
Toyoda Takeru <takerun20040214@gmail.com>
2026-08-04 11:10:43 +0900

    fix:Lintチェックで引っかかった箇所の修正 マイグレーションファイルの記述を修正

--- db/migrate/20260803165812_create_event_schedules.rb
+++ db/migrate/20260803165812_create_event_schedules.rb
@@ -3,7 +3,6 @@ class CreateEventSchedules < ActiveRecord::Migration[7.2]
     create_table :event_schedules do |t|
       t.references :event, null: false, foreign_key: true
       t.date :event_date, null: false
-      
       t.timestamps
     end
   end
