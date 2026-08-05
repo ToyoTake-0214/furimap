d2e6eac54a9f4811ec7de9e756cab4a351ff9f50
Toyoda Takeru <takerun20040214@gmail.com>
2026-08-05 23:04:40 +0900

    add:イベント削除機能のルーティングを追加 eventsのルーティングにdestroyを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "main_pages#top"
-  resources :events, only: [ :index, :new, :create, :edit, :update ]
+  resources :events, only: [ :index, :new, :create, :edit, :update, :destroy ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
