2b8d3df9845a492fb14c28f5d16bafc06a5b03a7
Toyoda Takeru <takerun20040214@gmail.com>
2026-09-03 23:24:00 +0900

    add:一般用のログインRoutingを追加
    - 一般ユーザー用の登録・ログインURL（例: /general/sign_up,/general/sign_in）を追加し、既存のUsers::RegistrationsController/Users::SessionsControllerにルーティングする

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get  "general/sign_up", to: "general/registrations#new",    as: :new_general_registration
     post "general/sign_up", to: "general/registrations#create", as: :general_registration
+
+    get  "general/sign_in", to: "general/sessions#new",    as: :new_general_session
+    post "general/sign_in", to: "general/sessions#create", as: :general_session
+  end
+
   end
 
   root "main_pages#top"
