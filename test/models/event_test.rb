b63e141b0c0a0d2520bb4f9a0a3844814993ca32
Toyoda Takeru <takerun20040214@gmail.com>
2026-09-09 13:56:02 +0900

    add:Eventの文字数バリデーション（境界値）のテストを追加 - name/description/address/event_timeについて、上限文字数を超えるとinvalidになることを確認するテストを追加 - 同じ4項目について、ちょうど上限文字数ならvalidであることを確認(境界値の両側を確認)

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -23,4 +23,52 @@ class EventTest < ActiveSupport::TestCase
     event.event_time = ""
     assert_not event.valid?, event.errors.full_messages.join(", ")
   end
+
+  test "nameが51文字以上の場合はinvalidであること" do
+    event = events(:one)
+    event.name = "a" * 51
+    assert_not event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "nameが50文字の場合はvalidであること" do
+    event = events(:one)
+    event.name = "a" * 50
+    assert event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "descriptionが2001文字以上の場合はinvalidであること" do
+    event = events(:one)
+    event.description = "a" * 2001
+    assert_not event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "descriptionが2000文字の場合はvalidであること" do
+    event = events(:one)
+    event.description = "a" * 2000
+    assert event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "addressが101文字以上の場合はinvalidであること" do
+    event = events(:one)
+    event.address = "a" * 101
+    assert_not event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "addressが100文字の場合はvalidであること" do
+    event = events(:one)
+    event.address = "a" * 100
+    assert event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "event_timeが51文字以上の場合はinvalidであること" do
+    event = events(:one)
+    event.event_time = "a" * 51
+    assert_not event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "event_timeが50文字の場合はvalidであること" do
+    event = events(:one)
+    event.event_time = "a" * 50
+    assert event.valid?, event.errors.full_messages.join(", ")
+  end
 end
