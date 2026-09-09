89df5237e44f885c8dbe91344f574516b2ed7ee1
Toyoda Takeru <takerun20040214@gmail.com>
2026-09-09 12:32:33 +0900

    add:Event必須項目(presence)の空チェックを追加 - nameが空の場合はinvalidであることを確認するテストを追加 - addressが空の場合はinvalidであることを確認するテストを追加 - event_timeが空の場合はinvalidであることを確認するテストを追加

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -5,4 +5,22 @@ class EventTest < ActiveSupport::TestCase
     event = events(:one)
     assert event.valid?, event.errors.full_messages.join(", ")
    end
+
+  test "nameが空の場合はinvalidであること" do
+    event = events(:one)
+    event.name = ""
+    assert_not event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "addressが空の場合はinvalidであること" do
+    event = events(:one)
+    event.address = ""
+    assert_not event.valid?, event.errors.full_messages.join(", ")
+  end
+
+  test "event_timeが空の場合はinvalidであること" do
+    event = events(:one)
+    event.event_time = ""
+    assert_not event.valid?, event.errors.full_messages.join(", ")
+  end
 end
